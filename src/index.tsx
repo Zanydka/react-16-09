fa5325ad58a8c61ceac407643140972ffb9b8831
Denis <marvelick753@gmail.com>
2022-12-23 22:36:06 +0200

    function react component

--- src/index.tsx
+++ src/index.tsx
@@ -11,8 +11,32 @@ import ReactDOM from 'react-dom/client'
 // )
 
 // with jsx
-const title = <h1 id="title">Hello React</h1>
-console.log(title)
+// const title = <h1 id="title">Hello React</h1>
+// console.log(title)
+
+// const content = (
+//     <div>
+//         <ul>
+//             <li>List item 1</li>
+//             <li>List item 2</li>
+//         </ul>
+//         <p>Hello world</p>
+//     </div>
+// )
+
+// function component
+function App() {
+    return (
+        <div>
+            <h1>Hello React</h1>
+            <p>lorem ipsum</p>
+        </div>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode>{title}</React.StrictMode>)
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
