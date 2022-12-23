befa68515e42276d5b9326f333668a19f3a532bc
Denis <marvelick753@gmail.com>
2022-12-24 01:36:54 +0200

    props

--- src/index.tsx
+++ src/index.tsx
@@ -34,10 +34,14 @@ import ReactDOM from 'react-dom/client'
 //     )
 // }
 
-const Title = () => {
-    return <h1>Hello React</h1>
+type TitleProps = {
+    title?: string | number
 }
 
+const Title = (props: TitleProps) => {
+    console.log(props)
+    return <h1>Hello {props.title}</h1>
+}
 const Content = () => {
     return (
         <React.Fragment>
@@ -59,7 +63,10 @@ const Content = () => {
 const App = () => {
     return (
         <>
-            <Title />
+            <Title title="React" />
+            <Title title="TS" />
+            <Title title="JS" />
+            <Title title={10} />
             <Content />
         </>
     )
