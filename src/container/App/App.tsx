b634701471397715653e443b04e2ead9e54b5860
Denis <marvelick753@gmail.com>
2022-12-26 10:56:17 +0200

    add className StyledEngineProvider

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -1,13 +1,13 @@
 import CssBaseline from '@mui/material/CssBaseline'
 import Header from 'container/Header/Header'
-
+import { StyledEngineProvider } from '@mui/material/styles'
 type Props = {}
 const App = (props: Props) => {
     return (
-        <>
+        <StyledEngineProvider injectFirst>
             <CssBaseline />
             <Header />
-        </>
+        </StyledEngineProvider>
     )
 }
 export default App
