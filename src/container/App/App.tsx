9fe8542b9dbd6c3b710e0c5bfc04f7cbd56fa852
Denis <marvelick753@gmail.com>
2023-01-02 20:25:34 +0200

    addProductToCart 1:2  2:3

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -13,15 +13,18 @@ const App = () => {
         2: 1,
     })
 
-    const addProductToCart = (count: number, price: number) => {}
+    const addProductToCart = (id: number, count: number) => {
+        setProductsInCart((prevState: ProductsInCart) => ({
+            ...prevState,
+            [id]: prevState[id] + count,
+        }))
+    }
 
     return (
         <StyledEngineProvider injectFirst>
             <CssBaseline />
             <Header productsInCart={productsInCart} />
-            <button onClick={() => addProductToCart(5, 500)}>
-                Add to cart
-            </button>
+            <button onClick={() => addProductToCart(1, 1)}>Add to cart</button>
             <Main addProductToCart={addProductToCart} />
         </StyledEngineProvider>
     )
