0dd50ac1f59e321d190bb41bf3689bfdb8597a57
Denis <marvelick753@gmail.com>
2023-01-18 00:48:36 +0200

    install redux

---
+++ src/redux/likeReducer.ts
@@ -0,0 +1,22 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+
+type LikeProducts = {
+    [id:number]: boolean
+}
+
+export const initialState:LikeProducts ={
+    1:true,
+    2:true
+}
+
+
+export const likeSlice = createSlice({
+    name:"like",
+    initialState,
+    reducers:{}
+})
+
+
+
+export default likeSlice.reducer
